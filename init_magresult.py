71baf0880d7cf90329eebb853ddaeda3bca1c9df
Ryan <ryan.felkai@gmail.com>
2017-02-25 11:41:32 +0100

    It's getting better.

--- init_magresult.py
+++ init_magresult.py
@@ -1,7 +1,7 @@
 import numpy as np
 import tables as tb
 
-f = tb.open_file("magresult.h5", "w", "Resulting data from Magboltz")
+f = tb.open_file("magresult.h5", "w", "Resulting data")
 
 f.flush()
 
